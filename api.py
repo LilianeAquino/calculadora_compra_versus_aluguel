5455c6b4b0fb0360481056fa88df342ebc57d64d
lilianelou <lilianelou@hotmail.com>
2023-07-30 08:43:23 -0300

    chore: adiciona tratamento de erro

--- api.py
+++ api.py
@@ -14,9 +14,9 @@ app.template_folder = 'templates'
 class FormCalc(FlaskForm):
     valor_mensal_aluguel = FloatField('Aluguel Mensal', validators=[InputRequired()])
     valor_compra_imovel = FloatField('Valor Imóvel', validators=[InputRequired()])
-    taxa_valorizacao_imovel = FloatField('Valorização Imóvel', validators=[InputRequired()])
-    taxa_aumento_aluguel = FloatField('Atualização Aluguel', validators=[InputRequired()])
-    taxa_juros_aplicacao = FloatField('Juros Aplicação', validators=[InputRequired()])
+    taxa_valorizacao_imovel = FloatField('Valorização Imóvel (em %)', validators=[InputRequired(), NumberRange(min=0)])
+    taxa_aumento_aluguel = FloatField('Atualização Aluguel (em %)', validators=[InputRequired(), NumberRange(min=0)])
+    taxa_juros_aplicacao = FloatField('Juros Aplicação (em %)', validators=[InputRequired(), NumberRange(min=0)])
     botao_calcular = SubmitField('Calcular')
 
 
